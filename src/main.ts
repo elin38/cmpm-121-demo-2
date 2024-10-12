c997225cda61821973bcc00ad6ad50b86cacceb9
Ethan Lin <eragon9356@gmail.com>
2024-10-12 16:27:34 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,42 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const clearHolder = document.createElement("div");
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+
+clearHolder.append(clearButton);
+app.append(clearHolder);
+
+const ctx = canvas.getContext("2d");
+const cursor = { active: false, x: 0, y: 0 };
+
+if (ctx) {
+    canvas.addEventListener("mousedown", (e) => {
+        cursor.active = true;
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    });
+
+    canvas.addEventListener("mousemove", (e) => {
+        if (cursor.active) {
+            ctx.beginPath();
+            ctx.moveTo(cursor.x, cursor.y);
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+            cursor.x = e.offsetX;
+            cursor.y = e.offsetY;
+        }
+    });
+
+    canvas.addEventListener("mouseup", (e) => {
+        cursor.active = false;
+    });
+
+    clearButton.addEventListener("click", () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+} else {
+    console.error("Unable to get canvas 2D context");
+}
