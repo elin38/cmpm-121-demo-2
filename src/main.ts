f3baf7f131449b45b6b4e56d1408408031832b5d
Ethan Lin <eragon9356@gmail.com>
2024-10-17 22:16:57 -0700

    Tried to refactor Step 6's DOM manipulation code

--- src/main.ts
+++ src/main.ts
@@ -9,37 +9,51 @@ const appTitle = document.createElement("h1");
 appTitle.innerHTML = APP_NAME;
 app.append(appTitle);
 
-// Create canvas
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
 
-// Create buttons for clear, undo, redo
+function createButton(label: string, parent: HTMLElement, onClick: () => void): HTMLButtonElement {
+  const button = document.createElement("button");
+  button.innerHTML = label;
+  button.addEventListener("click", onClick);
+  parent.append(button);
+  return button;
+}
+
 const buttonHolder = document.createElement("div");
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "Clear";
-buttonHolder.append(clearButton);
-
-const undoButton = document.createElement("button");
-undoButton.innerHTML = "Undo";
-buttonHolder.append(undoButton);
-
-const redoButton = document.createElement("button");
-redoButton.innerHTML = "Redo";
-buttonHolder.append(redoButton);
-
+createButton("Clear", buttonHolder, () => {
+  lines = [];
+  redoLines = [];
+  dispatchDrawingChanged();
+});
+createButton("Undo", buttonHolder, () => {
+  if (lines.length > 0) {
+    redoLines.push(lines.pop()!);
+    dispatchDrawingChanged();
+  }
+});
+createButton("Redo", buttonHolder, () => {
+  if (redoLines.length > 0) {
+    lines.push(redoLines.pop()!);
+    dispatchDrawingChanged();
+  }
+});
 app.append(buttonHolder);
 
 const thicknessButtonHolder = document.createElement("div");
-const thinButton = document.createElement("button");
-thinButton.innerHTML = "Thin";
-thicknessButtonHolder.append(thinButton);
-
-const thickButton = document.createElement("button");
-thickButton.innerHTML = "Thick";
-thicknessButtonHolder.append(thickButton);
-
+const thinButton = createButton("Thin", thicknessButtonHolder, () => {
+  lineThickness = 1;
+  thinButton.classList.add("selectedTool");
+  thickButton.classList.remove("selectedTool");
+});
+const thickButton = createButton("Thick", thicknessButtonHolder, () => {
+  lineThickness = 4;
+  thickButton.classList.add("selectedTool");
+  thinButton.classList.remove("selectedTool");
+});
+thicknessButtonHolder.append(thinButton, thickButton);
 app.append(thicknessButtonHolder);
 
 const ctx = canvas.getContext("2d");
@@ -94,42 +108,11 @@ if (ctx) {
     currentLine = null;
     dispatchDrawingChanged();
   });
-
-  clearButton.addEventListener("click", () => {
-    lines = [];
-    redoLines = [];
-    dispatchDrawingChanged();
-  });
-
-  undoButton.addEventListener("click", () => {
-    if (lines.length > 0) {
-      redoLines.push(lines.pop()!);
-      dispatchDrawingChanged();
-    }
-  });
-
-  redoButton.addEventListener("click", () => {
-    if (redoLines.length > 0) {
-      lines.push(redoLines.pop()!);
-      dispatchDrawingChanged();
-    }
-  });
-
-  thinButton.addEventListener("click", () => {
-    lineThickness = 1;
-    thinButton.classList.add("selectedTool");
-    thickButton.classList.remove("selectedTool");
-  });
-
-  thickButton.addEventListener("click", () => {
-    lineThickness = 4;
-    thickButton.classList.add("selectedTool");
-    thinButton.classList.remove("selectedTool"); 
-  });
 } else {
   console.error("Unable to get canvas 2D context");
 }
 
+// written with the help of chatGPT
 class Line {
   private points: { x: number; y: number }[];
   private thickness: number;
@@ -140,7 +123,7 @@ class Line {
   }
 
   display(ctx: CanvasRenderingContext2D) {
-    ctx.lineWidth = this.thickness; 
+    ctx.lineWidth = this.thickness;
     ctx.beginPath();
     const { x, y } = this.points[0];
     ctx.moveTo(x, y);
